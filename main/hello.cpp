4b86de903a18824e86824d62fad530b4124aa586
Niveditar11 <117768875+Niveditar11@users.noreply.github.com>
2023-02-20 10:01:02 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -5,6 +5,6 @@ using namespace std;
 int main(void) {
   cout << "Hello, nivedita\n";
   cout << "Hello, Jenkins\n";
-  cout << "I have successfully built and run\n";
+  cout << "PES2UG21CS811 have successfully built and run\n";
   return 0;
 }
